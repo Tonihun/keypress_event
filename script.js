f8e85f3447d9111036a716706665fb13d288940b
avaradikrisz17 <kvaradi08@gmail.com>
2025-02-24 11:59:00 +0100

    változás a scriptben

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 window.addEventListener('DOMContentLoaded', () => {
-const containerDiv = document.createElement("div")
-containerDiv.classList.add('container')
+const containerDiv = document.createElement("div");
+containerDiv.classList.add('container');
 
-const demoSpan = document.createElement('span')
-demoSpan.id = 'demo'
+const demoSpan = document.createElement('span');
+demoSpan.id = 'demo';
 
-const textarea = document.createElement('textarea')
-textarea.name = 'area'
-textarea.id='area'
+const textarea = document.createElement('textarea');
+textarea.name = 'area';
+textarea.id='area';
 
 textarea.placeholder = 'Írd be a jelszavad:'
 
+textarea.addEventListener('keypress', () => jelszo(textarea, demoSpan));
 
 
+containerDiv.append(demoSpan, textarea);
 
-containerDiv.append(demoSpan, textarea)
-
-document.body.append(containerDiv)
+document.body.append(containerDiv);
 
 
 })
+
+function jelszo(textarea, demoSpan) {
+    let csillag = ''
+
+    for (let i= 0, i < textarea.value.length; i++) {
+
+    }
+}
